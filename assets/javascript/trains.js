8bafb7870a5d91b11ccf8e530a1751de7b4c5fab
NelsonCad <NelsonCad97@gmail.com>
2019-07-30 14:38:13 -0700

    added ground work for homework

---
+++ assets/javascript/trains.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyCezcaWLzFQUKIukuWYcnaND4MsxzN_H9I",
+    authDomain: "iliketrains-e7564.firebaseapp.com",
+    databaseURL: "https://iliketrains-e7564.firebaseio.com",
+    projectId: "iliketrains-e7564",
+    storageBucket: "",
+    messagingSenderId: "425659136502",
+    appId: "1:425659136502:web:aa4bb59f375d75c8"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
